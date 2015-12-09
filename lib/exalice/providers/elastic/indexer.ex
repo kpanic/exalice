63fe4a66f43f6f15887aea2af823cece646ca241
Marco Milanesi <kpanic@gmail.com>
2015-12-09 09:19:22 +0000

    Re-organize code

--- lib/exalice/providers/elastic/indexer.ex
+++ lib/exalice/providers/elastic/indexer.ex
@@ -13,18 +13,49 @@ defmodule ExAlice.Geocoder.Providers.Elastic.Indexer do
   end
 
   defp prepare_doc(docs) when is_list(docs) do
-    Enum.map(docs, fn doc -> [{@index_name, "location", UUID.uuid4(),
-        Map.to_list(doc)}] end)
+    Enum.map(docs, fn doc ->
+      [{"exalice", "location", UUID.uuid4(), Map.to_list(filter_doc(doc))}]
+    end
+    )
   end
 
   defp prepare_doc(doc) do
-    [{@index_name, "location", UUID.uuid4(), Map.to_list(doc)}]
+    [{"exalice", "location", UUID.uuid4(), Map.to_list(filter_doc(doc))}]
+  end
+
+  defp filter_doc(%{"lat" => lat, "lon" => lon,
+    "tags" =>
+      %{"addr:city" => city, "addr:country" => country,
+        "addr:housenumber" => housenumber, "addr:postcode" => postcode,
+        "addr:street" => street}
+    }) do
+
+    %{lat: lat, lon: lon,
+      location: %{city: city, state: country,
+        street: street, housenumber: housenumber,
+        postcode: postcode}}
+  end
+
+  defp filter_doc(%{lat: lat, lon: lon,
+     location: %{city: city, country: country,
+       housenumber: housenumber, state: state,
+       postcode: postcode, street: street},
+     }) do
+
+    %{lat: lat, lon: lon,
+      location: %{city: city, country: country,
+        street: street, housenumber: housenumber,
+        postcode: postcode, state: state}}
+  end
+
+  # Discard not "pure" docs :)
+  defp filter_doc(_) do
+    %{}
   end
 
   defp index_docs(docs) do
     params = erls_params()
     docs = List.flatten(docs)
-    # TODO: Convert docs to a common format before indexing
     :erlastic_search.bulk_index_docs(params, docs)
   end
 end
