041f35bc0f4d0beac73943e4d308e50f302d5f1b
Marco Milanesi <kpanic@gmail.com>
2015-12-09 21:12:15 +0000

    Remove binary

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule ExAlice.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     escript: [main_module: Importer],
      deps: deps]
   end
 
