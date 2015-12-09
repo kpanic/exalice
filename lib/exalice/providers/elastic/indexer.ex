f7464283e505969d38570b451c517ee8c1c3f95e
Marco Milanesi <kpanic@gmail.com>
2015-12-09 17:32:29 +0000

    Filter "non pure" docs :)

--- lib/exalice/providers/elastic/indexer.ex
+++ lib/exalice/providers/elastic/indexer.ex
@@ -13,14 +13,16 @@ defmodule ExAlice.Geocoder.Providers.Elastic.Indexer do
   end
 
   defp prepare_doc(docs) when is_list(docs) do
-    Enum.map(docs, fn doc ->
+    Stream.map(docs, fn doc ->
       [{@index_name, "location", UUID.uuid4(), Map.to_list(filter_doc(doc))}]
     end
     )
+    |> Enum.filter(fn(x) -> x != [] end)
   end
 
   defp prepare_doc(doc) do
     [{@index_name, "location", UUID.uuid4(), Map.to_list(filter_doc(doc))}]
+    |> Enum.filter(fn(x) -> x != %{} end)
   end
 
   defp filter_doc(%{"lat" => lat, "lon" => lon,
