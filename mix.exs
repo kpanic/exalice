ba4cbe72731ffefd247afdad11f6cf3349abf931
Marco Milanesi <kpanic@gmail.com>
2015-12-21 20:37:55 +0000

    Add tirexs as a dependency

--- mix.exs
+++ mix.exs
@@ -31,11 +31,11 @@ defmodule ExAlice.Mixfile do
     [
       {:poison, "~> 1.5.0"},
       {:httpoison, "~> 0.8"},
-      {:erlastic_search, "~> 1.0.0"},
       {:hackney, "~> 1.4.4", [optional: false, hex: :hackney, override: true]},
       {:towel, "~> 0.2"},
       {:uuid, "~> 1.1.1"},
-      {:credo, "~> 0.1.9", only: [:dev, :test]}
+      {:credo, "~> 0.1.9", only: [:dev, :test]},
+      {:tirexs, "~> 0.7.3"}
     ]
   end
 end
