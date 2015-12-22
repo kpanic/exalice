5174c27621adb43dd74c2a85a29643e1e2c2b4a1
Marco Milanesi <kpanic@gmail.com>
2015-12-22 17:26:53 +0000

    Remove the hackney hack

--- lib/tasks/bootstrap.ex
+++ lib/tasks/bootstrap.ex
@@ -9,11 +9,6 @@ defmodule Mix.Tasks.Exalice.Bootstrap do
   """
 
   def run(args) do
-    # NOTE: Hackney has to be started manually in the task
-    # it seems that mix.exs is not honoured, hackney should be started by
-    # :erlastic_search since it's a dependency of it?
-    :hackney.start()
-
     # FIXME: works only with fixed argument
     cond do
       args == [] ->
