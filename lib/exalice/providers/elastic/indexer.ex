125483ea803136f56d00d14c8b48af6ca7e30464
Marco Milanesi <kpanic@gmail.com>
2015-12-22 17:25:53 +0000

    Everything becomes a Keyword

--- lib/exalice/providers/elastic/indexer.ex
+++ lib/exalice/providers/elastic/indexer.ex
@@ -16,12 +16,19 @@ defmodule ExAlice.Geocoder.Providers.Elastic.Indexer do
 
   defp prepare_doc(docs) when is_list(docs) do
     Stream.map(docs, fn doc ->
-      {@index_name, "location", UUID.uuid4(), Map.to_list(filter_doc(doc))}
+      doc = filter_doc(doc)
+      metadata = [{:type, "location"}, {:_id, UUID.uuid4()}]
+      merged_doc = Keyword.merge(doc, metadata)
+      [{:index, merged_doc}]
     end)
   end
 
   defp prepare_doc(doc) do
-    {@index_name, "location", UUID.uuid4(), Map.to_list(filter_doc(doc))}
+      doc = filter_doc(doc)
+      IO.inspect doc
+      metadata = [{:type, "location"}, {:_id, UUID.uuid4()}]
+      merged_doc = Keyword.merge(doc, metadata)
+      [{:index, merged_doc}]
   end
 
   defp filter_doc(%{"lat" => lat, "lon" => lon,
@@ -31,10 +38,11 @@ defmodule ExAlice.Geocoder.Providers.Elastic.Indexer do
         "addr:street" => street}
     }) do
 
-    %{lat: lat, lon: lon,
-      location: %{city: city, state: country,
-        street: street, housenumber: housenumber,
-        postcode: postcode}}
+    coordinates = [{:lat, lat}, {:lon, lon}]
+    location = [{:location, [{:city, city}, {:state, country},
+        {:street, street}, {:housenumber, housenumber},
+        {:postcode, postcode}]}]
+    Keyword.merge(coordinates, location)
   end
 
   defp filter_doc(%{lat: lat, lon: lon,
@@ -43,20 +51,21 @@ defmodule ExAlice.Geocoder.Providers.Elastic.Indexer do
        postcode: postcode, street: street},
      }) do
 
-    %{lat: lat, lon: lon,
-      location: %{city: city, country: country,
-        street: street, housenumber: housenumber,
-        postcode: postcode, state: state}}
+    coordinates = [{:lat, lat}, {:lon, lon}]
+    location = [{:location, [{:city, city}, {:country, country},
+          {:street, street}, {:housenumber, housenumber},
+          {:postcode, postcode}, {:state, state}]}]
+    Keyword.merge(coordinates, location)
   end
 
   defp filter_doc(_) do
-    %{}
+    []
   end
 
   defp index_docs(docs) do
     docs
     # Discard not "pure" docs :)
-    |> Enum.filter(fn(x) -> Enum.at(Tuple.to_list(x), 3) != [] end)
+    |> Enum.filter(fn(x) -> Enum.count(x) != 2 end)
     |> bulk_index
   end
 
