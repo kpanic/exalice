e8548a35965757127dc36c5a18b8b49ef08a3536
Marco Milanesi <kpanic@gmail.com>
2015-12-26 17:58:38 +0100

    Some initial tests

--- test/exalice_test.exs
+++ test/exalice_test.exs
@@ -1,8 +1,68 @@
 defmodule ExAliceTest do
   use ExUnit.Case
+
+  import Tirexs.Search
+  require Tirexs.Query
+  require Tirexs.ElasticSearch
+
+  import ExAlice.Geocoder.Providers.Elastic.Importer, only: [
+    read_file: 1, json_decode: 1, index: 1, chunk: 2]
+
+
   doctest ExAlice
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def setup_all do
+    index_name = ExAlice.Geocoder.config(:index)
+    settings = Tirexs.ElasticSearch.config()
+    Tirexs.ElasticSearch.delete(index_name, settings)
+    Tirexs.ElasticSearch.put(index_name, settings)
+    :ok
+  end
+
+  def indexing_prewarming(chunk_number \\ 10) do
+    file = ExAlice.Geocoder.config(:file)
+    read_file(file)
+    |> chunk(chunk_number)
+    |> json_decode
+  end
+
+  test "expect that the chunks are split correctly" do
+    # total chunks are 10 in data/test-data.json
+    chunk_number = 2
+
+    res = indexing_prewarming(chunk_number)
+
+    assert Enum.count(res) == 5
+  end
+
+
+  test "expect that \"oversize\" chunks are split correctly" do
+    # total chunks are 10 in data/test-data.json
+    chunk_number = 100
+
+    res = indexing_prewarming(chunk_number)
+
+    assert Enum.count(res) == 1
+  end
+
+  test "expect that the chunk is indexed and split correctly" do
+    chunk_number = 3
+    indexing_prewarming
+    |> index
+    # wait for indexing time
+    :timer.sleep 1000
+
+    index_name = ExAlice.Geocoder.config(:index)
+
+    query = search [index: index_name] do
+              query do
+                string "*"
+              end
+            end
+
+    result = Tirexs.Query.create_resource(query)
+    result = Tirexs.Query.result(result, :hits)
+
+    assert Enum.count(List.flatten(result)) == 10
   end
 end
