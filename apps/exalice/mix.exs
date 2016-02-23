723306921b989b47e1ced784dc6c668f7c60b0eb
Marco Milanesi <kpanic@gmail.com>
2016-02-23 20:58:55 +0100

    Move ExAlice to an umbrella project

---
+++ apps/exalice/mix.exs
@@ -0,0 +1,46 @@
+defmodule ExAlice.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :exalice,
+     version: "0.0.1",
+     build_path: "../../_build",
+     config_path: "../../config/config.exs",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
+     elixir: "~> 1.2",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger, :httpoison],
+     mod: {ExAlice, []}]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [
+      {:poison, "~> 1.5.0"},
+      {:httpoison, "~> 0.8"},
+      {:hackney, "~> 1.4.4", [optional: false, hex: :hackney, override: true]},
+      {:towel, "~> 0.2"},
+      {:uuid, "~> 1.1.1"},
+      {:credo, "~> 0.1.9", only: [:dev, :test]},
+      {:tirexs, "~> 0.7.4"}
+    ]
+  end
+end
--- mix.exs
+++ mix.exs
@@ -1,21 +1,11 @@
-defmodule ExAlice.Mixfile do
+defmodule ExaliceApps.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :exalice,
-     version: "0.0.1",
-     elixir: "~> 1.2.1",
+    [apps_path: "apps",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
-  end
-
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger, :httpoison],
-     mod: {ExAlice, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -26,17 +16,12 @@ defmodule ExAlice.Mixfile do
   #
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
-  # Type "mix help deps" for more examples and options
+  # Type "mix help deps" for more examples and options.
+  #
+  # Dependencies listed here are available only for this project
+  # and cannot be accessed from applications inside the apps folder
   defp deps do
     [
-      {:poison, "~> 1.5.0"},
-      {:httpoison, "~> 0.8"},
-      {:hackney, "~> 1.4.4", [optional: false, hex: :hackney, override: true]},
-      {:towel, "~> 0.2"},
-      {:uuid, "~> 1.1.1"},
-      {:credo, "~> 0.1.9", only: [:dev, :test]},
-      {:tirexs, "~> 0.7.4"}
-
     ]
   end
 end
