880bdf4d5cc51323dbc9baa2fd6e4a67f89c780c
Marco Milanesi <kpanic@gmail.com>
2016-03-17 19:51:53 +0100

    Add capacity in configurations

--- config/dev.exs
+++ config/dev.exs
@@ -3,5 +3,6 @@ use Mix.Config
 config :exalice,
   index: :exalice,
   doc_type: :location,
-  file: "data/germany-streets.json",
+  capacity: 4,
+  file: "data/germany-streets-full.json",
   chunks: 5000
--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,6 @@ use Mix.Config
 config :exalice,
   index: :exalice,
   doc_type: :location,
+  capacity: 4,
   file: "data/germany-streets.json",
   chunks: 5000
