b1ac61df1849fb77cd156ffe5967701ae53e6ee7
Marco Milanesi <kpanic@gmail.com>
2016-08-28 18:02:44 +0200

    Remove the workers

--- lib/exalice.ex
+++ lib/exalice.ex
@@ -4,10 +4,7 @@ defmodule ExAlice do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    children = [
-      worker(ExAlice.Worker, []),
-      worker(ExAlice.StreamRunner, [4, [name: ExAlice.StreamRunner]])
-    ]
+    children = []
 
     opts = [strategy: :one_for_one, name: ExAlice.Supervisor]
     Supervisor.start_link(children, opts)
